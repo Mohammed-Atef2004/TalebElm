106f1e47cb3fc962d948f6dff5f8d77233b9ad3b
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-09-03 01:26:21 +0300

    feat : add ProgressResponse Recode

---
+++ src/TalebElm.Application/DTOs/ProgressResponse.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Application.DTOs
+{
+    public record ProgressResponse(Guid ModuleId, bool IsUnlocked, bool PassedExam, int Score);
+}

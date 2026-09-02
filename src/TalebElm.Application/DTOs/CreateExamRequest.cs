070e1287ad04b38f503a38ebbe0b3ae420cdabb5
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-09-03 01:45:22 +0300

    feat: add CreateExamRequest

---
+++ src/TalebElm.Application/DTOs/CreateExamRequest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TalebElm.Application.DTOs
+{
+    public record CreateExamRequest(Guid ModuleId, string Title, int PassThreshold);
+}

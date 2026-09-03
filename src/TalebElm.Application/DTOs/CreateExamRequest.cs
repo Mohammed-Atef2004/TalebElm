6126a703957c0a6835f3c7df07289073dbde9f5d
abdulrahman hossam <abdulrahmn398@outlook.com>
2026-09-03 19:39:25 +0300

    Clean up using directives in CreateExamRequest.cs
    Removed unnecessary using directives from CreateExamRequest.cs.

--- src/TalebElm.Application/DTOs/CreateExamRequest.cs
+++ src/TalebElm.Application/DTOs/CreateExamRequest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TalebElm.Application.DTOs
+﻿namespace TalebElm.Application.DTOs
 {
     public record CreateExamRequest(Guid ModuleId, string Title, int PassThreshold);
 }
